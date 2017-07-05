f5984c4ea93116cc613ae01a3404ecbcc20ab145
nambingo <phamducnamtb@gmail.com>
2017-07-05 11:19:08 +0700

    create left menu

---
+++ MovieDBTraining/app/src/main/java/com/framgia/moviedbtraining/search/SearchActivity.kt
@@ -0,0 +1,16 @@
+package com.framgia.moviedbtraining.search
+
+import android.app.Activity
+import android.os.Bundle
+import com.framgia.moviedbtraining.R
+
+/**
+ * Created by FRAMGIA\pham.duc.nam on 05/07/2017.
+ */
+
+class SearchActivity : Activity(){
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_search)
+  }
+}

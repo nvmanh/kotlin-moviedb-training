c9b5fdc09a1ae771adeea5907cf6825a58b9a966
babatunde1724 <babatunde1724@gmail.com>
2017-07-05 15:40:28 +0700

    setup Api

---
+++ MovieDBTraining/app/src/main/java/com/framgia/moviedbtraining/constants/Constants.kt
@@ -0,0 +1,11 @@
+package com.framgia.moviedbtraining.constants
+ interface Constants {
+  companion object {
+    val API_KEY = "bb4a8f3243d4c0015221df83096fd63a"
+    val WEB_URL = "http://image.tmdb.org/t/p/w500"
+    val EXTRA_MOVIE = "extra_movie"
+    val IMAGE = "img"
+    val NUM_VISIBLE_THRESOLD = 3
+    val ONE_PAGE_WITH_20 = 20
+  }
+}

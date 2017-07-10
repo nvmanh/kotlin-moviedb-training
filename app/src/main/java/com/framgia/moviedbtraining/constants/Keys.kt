d0afe9d951f205e41f1e9f7f2dbf6746bfd870d5
babatunde1724 <babatunde1724@gmail.com>
2017-07-10 16:23:02 +0700

    User Profile Screen

--- app/src/main/java/com/framgia/moviedbtraining/constants/Keys.kt
+++ app/src/main/java/com/framgia/moviedbtraining/constants/Keys.kt
@@ -4,5 +4,6 @@ package com.framgia.moviedbtraining.constants
        val APPLICATION_PREFS = "app_preference"
        val SESSION_ID = "session_id"
        val  IS_LOGIN = "is_login"
+       val  USER = "user"
      }
 }
